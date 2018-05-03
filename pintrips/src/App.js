71dd6973c0a3af0afee5e9237f13e1be32449c2d
Alexa Moy <s.alexa.moy@gmail.com>
2018-05-03 16:52:52 -0400

    some react-redux things

--- pintrips/src/App.js
+++ pintrips/src/App.js
@@ -11,30 +11,8 @@ firebase.initializeApp({
   projectId: "pintrips-4e855"
 });
 // Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
+let db = firebase.firestore();
 
-db.collection('users').add({
-  first: 'Ada', 
-  last: 'Lovelace',
-  born: 1815
-})
-.then(function(docRef) {
-  console.log('Document written with ID: ', docRef.id);
-})
-.catch(function(error) {
-  console.error('Error adding document: ', error);
-})
-
-db.collection("users").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-  });
-});
-
-db.collection('boards').doc()
-.collection('pins').add({
-  date: 'Thursday'
-})
 class App extends Component {
   render() {
     return (
--- pintrips/src/index.js
+++ pintrips/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// import { Router } from 'react-router-dom';
+import store from './client/store';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
