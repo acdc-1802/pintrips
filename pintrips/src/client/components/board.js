9892f63ebb05f8a99f9c003c2f058230b2f8a053
Destin McMurry <destinmcmurry@gmail.com>
2018-05-04 16:39:14 -0400

    showing map

--- pintrips/src/client/components/board.js
+++ pintrips/src/client/components/board.js
@@ -3,18 +3,32 @@ const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 class Board extends Component {
 
+  state = {
+    center: [-74.010190, 40.705515]
+  }
+
   componentDidMount() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZGVzdGlubWNtdXJycnkiLCJhIjoiY2plenRxaGw3MGdsNTJ3b2htMGRydWc3aiJ9.ycslnjgv2J9VZGZHT8EoIw';
-    const map = new mapboxgl.Map({
+    new mapboxgl.Map({
       container: 'map',
+      center: this.state.center,
+      zoom: 13,
       style: 'mapbox://styles/destinmcmurrry/cjgrf7lo100062so52bmvhjkm'
     });
   }
 
   render() {
     return (
-      <div>
+      <div className='board-container'>
         <div id='map'></div>
+        <div className='map-overlay'> 
+          <div>
+          
+          </div>
+          <div className='footer'>
+            <p id='expand-up'>-</p>
+          </div>
+        </div>
       </div>
     )
   }
