ebc9ad176ac2fa39b4014078c999240cba307d67
Crystal Magloire <crys.magloire@gmail.com>
2018-05-05 13:51:19 -0400

    working

--- pintrips/src/client/components/HomePage.js
+++ pintrips/src/client/components/HomePage.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import { Input } from 'react-blur-admin';
+import {Row, Col} from 'react-flex-proto';
 
 class HomePage extends Component {
     render() {
         return (
-          <div className="home-page">
-            <Sidebar />
-            <div className="home-main">
-              <div className="board">
-                <h3> Paris </h3> 
-                <img className="image" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6wpTX_Re1488fB2Lq5ngmN3i5f83p5Vmhk7JOjPccOLmByGv4"}/>
-              </div> 
-              <div className="board">
-                <h3> Paris </h3> 
-                <img className="image" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6wpTX_Re1488fB2Lq5ngmN3i5f83p5Vmhk7JOjPccOLmByGv4"}/>
-              </div> 
-              <div className="board">
-                <h3> Paris </h3> 
-                <img className="image" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6wpTX_Re1488fB2Lq5ngmN3i5f83p5Vmhk7JOjPccOLmByGv4"}/>
-              </div> 
-            </div>
+          <div>
           </div>
         );
     }
 }
 
 export default HomePage;
+
+
+
+
+// <div className="home-page" >
+//             <Sidebar />
+//             <div className="home-main">
+//             <Input
+//             onChange={e => this.onTextChange('warning', e)}
+//             onValidate={e => 'warning'}
+//             label='Warning'
+//             value={this.state.warning} />
+//               <div className="board">
+//                 <h3> Paris </h3> 
+//                 <img className="image" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6wpTX_Re1488fB2Lq5ngmN3i5f83p5Vmhk7JOjPccOLmByGv4"}/>
+//               </div> 
+//               <div className="board">
+//                 <h3> Paris </h3> 
+//                 <img className="image" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6wpTX_Re1488fB2Lq5ngmN3i5f83p5Vmhk7JOjPccOLmByGv4"}/>
+//               </div> 
+//             </div>
+//           // </div>
--- pintrips/src/client/components/index.js
+++ pintrips/src/client/components/index.js
@@ -1,7 +1,8 @@
 
-export { default as Board } from './board';
+export { default as Board } from './Board';
 export { default as HomePage } from './HomePage';
 export { default as LoginPage } from './LoginPage';
 export { default as Navbar } from './Navbar';
 export { default as SignupPage } from './SignupPage';
 export { default as AddNewBoard } from './AddNewBoard';
+
