bf1b94eb315e1d6bbc49c45bb35bf4a2f3ce1e4b
Alexa Moy <s.alexa.moy@gmail.com>
2018-05-05 20:15:32 -0400

    mapped all boards

--- pintrips/src/client/components/MapCard.js
+++ pintrips/src/client/components/MapCard.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Button, Card, Image, Icon} from 'semantic-ui-react'
 
 class MapCard extends Component {
+  constructor(props){
+    super(props)
+  }
   render() {
     return (
       <div className='ind-card'>
@@ -9,18 +12,18 @@ class MapCard extends Component {
       <Image src='http://geoawesomeness.com/wp-content/uploads/2016/02/Paris-map.png' />
       <Card.Content>
         <Card.Header>
-          Paris
+          {this.props.board.name}
         </Card.Header>
         <Card.Meta>
           <span className='date'>
-            Started 
+            {this.props.board.locked}
           </span>
         </Card.Meta>
+        </Card.Content>
+        <Card.Content extra>
         <Card.Description>
           Note avout travel
         </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
        
       </Card.Content>
     </Card>
