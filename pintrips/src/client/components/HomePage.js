534e052ef9751dd412f86822d28974658ecd2dcb
Crystal Magloire <crys.magloire@gmail.com>
2018-05-07 11:52:54 -0400

    new homepage

--- pintrips/src/client/components/HomePage.js
+++ pintrips/src/client/components/HomePage.js
@@ -7,32 +7,7 @@ import db from '../firestore';
 import { Map, withAuth } from 'fireview';
 
 const allBoards = db.collection('boards')
-var networkDataReceived = false;
 
-startSpinner();
-
-// fetch fresh data
-var networkUpdate = fetch('/data.json').then(function(response) {
-  return response.json();
-}).then(function(data) {
-  networkDataReceived = true;
-  updatePage();
-});
-
-// fetch cached data
-caches.match('/data.json').then(function(response) {
-  if (!response) throw Error("No data");
-  return response.json();
-}).then(function(data) {
-  // don't overwrite newer network data
-  if (!networkDataReceived) {
-    updatePage(data);
-  }
-}).catch(function() {
-  // we didn't get cached data, the network is our last hope:
-  return networkUpdate;
-}).catch(showErrorMessage).then(stopSpinner);
-console.log('caches')
 const HomePage = (props) => {
   
   const user = props._user;
@@ -53,7 +28,6 @@ const HomePage = (props) => {
 }
 
 export default withAuth(HomePage);
-
 
 
 
