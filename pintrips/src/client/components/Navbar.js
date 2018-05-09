23cd5779568665a750cc1e3e1402657e8ed45b48
Destin McMurry <destinmcmurry@gmail.com>
2018-05-09 19:35:57 -0400

    small css

--- pintrips/src/client/components/Navbar.js
+++ pintrips/src/client/components/Navbar.js
@@ -22,40 +22,41 @@ const Navbar = props => {
         </Link>
         <h1 id='name'>Pintrips</h1>
         {
-        user &&
+          user &&
           (
-          <div className='user-nav'>
-            <small id='email'>{user.email}</small>
-            <a id='logout' href='#' onClick={handleLogout}>Logout</a>
-          </div>
+            <div className='user-nav'>
+              <small id='email'>{user.email}</small>
+              <a id='logout' href='#' onClick={handleLogout}>Logout</a>
+            </div>
           )
         }
       </div>
       {
-      user &&
-      (
-        <Menu className='sub-navbar' borderless={true}>
-          <Link to={'/HomePage'}>
-            <Menu.Item id='dropdown'>
-              My Boards
-          </Menu.Item>
-          </Link>
-          <Link to={'/SharedWithMe'}>
-            <Menu.Item borderless={true} id='dropdown'>
-              Shared With Me
-          </Menu.Item>
-          </Link>
-          <Link to={'/AddNewBoard'}>
-            <Menu.Item id='create-btn'>
-              Create New</Menu.Item>
-          </Link>
-          <Link to={'/SharedWithMe'}>
-            <Menu.Item borderless={true} id='navbar-notifications'>
-              <Icon name='bell outline' size='medium' />
-            </Menu.Item>
-          </Link>
-        </Menu>
-      )
+        user &&
+        (
+          <Menu className='sub-navbar' borderless={true}>
+            <Link to={'/HomePage'}>
+              <Menu.Item id='dropdown'>
+                My Boards
+              </Menu.Item>
+            </Link>
+            <Link to={'/SharedWithMe'}>
+              <Menu.Item borderless={true} id='dropdown'>
+                Shared With Me
+              </Menu.Item>
+            </Link>
+            <Link to={'/AddNewBoard'}>
+              <Menu.Item id='create-btn'>
+                Create New
+              </Menu.Item>
+            </Link>
+            <Link to={'/SharedWithMe'}>
+              <Menu.Item borderless={true} id='navbar-notifications'>
+                <Icon name='bell outline' size='medium' />
+              </Menu.Item>
+            </Link>
+          </Menu>
+        )
       }
     </div>
   );
