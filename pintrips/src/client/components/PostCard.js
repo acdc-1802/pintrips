814146a06d54bb40c9fdac3480b96a59b072d1ba
Cecilia Avery <cilsong@gmail.com>
2018-05-09 21:33:22 -0400

    current locations saves to db

--- pintrips/src/client/components/PostCard.js
+++ pintrips/src/client/components/PostCard.js
@@ -38,35 +38,26 @@ export class PostCard extends Component {
     const user = db.collection("users").doc(userId)
     let self = this;
     navigator.geolocation.getCurrentPosition(function(position) {
+      user.set({
+        currentCoordinates: new firebase.firestore.GeoPoint(position.coords.latitude, position.coords.longitude)
+        }, { merge: true })
       self.setState({
         currentCoordinates: [position.coords.latitude, position.coords.longitude]
       });
     },
       (err) => alert(err.message)
     );
-
-    this.state.currentCoordinates.length &&
-    user.set({
-      currentCoordinates: new firebase.firestore.GeoPoint(this.state.currentCoordinates[0], this.state.currentCoordinates[1])
-      }, { merge: true })
-      .then(() => {
-        console.log("User's current location updated")
-      })
-      .catch((err) => {
-        console.error("Error updating the current location: ", err)
-    })
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.state.currentCoordinates !== nextState.currentCoordinates
   }
 
   render() {
+    console.log('state?', this.state)
     return (
-      // <SingleBoard />
-      <h1>here.</h1>
+      <div className="login-container">
+        <h1>here.</h1>
+      </div>
     )
   }
+
 }
 
 export default withAuth(PostCard)
