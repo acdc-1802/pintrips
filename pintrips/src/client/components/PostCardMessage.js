c385f3595eb0551319e469dcc9e4fe6a6a7dbe83
Cecilia Avery <cilsong@gmail.com>
2018-05-12 15:44:32 -0400

    user form feedback updates

--- pintrips/src/client/components/PostCardMessage.js
+++ pintrips/src/client/components/PostCardMessage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import firebase from 'firebase'
 import db from '../firestore';
 import { Map, withAuth } from 'fireview';
-import { Form, TextArea, Button, Icon } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 import PostCardStamp from './PostCardStamp';
 
 require('firebase/firestore');
@@ -12,12 +12,12 @@ export class PostCard extends Component {
     super(props);
     this.state = {
       currentCoordinates: [],
-      city: '',
-      state: '',
-      country: '',
-      date: new Date().toLocaleDateString(),
-      sentPostcard: false
+      receiverEmail: '',
+      postCardBody: '',
+      senderEmail: ''
     }
+
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
@@ -25,6 +25,17 @@ export class PostCard extends Component {
       currentCoordinates: this.props.currentCoord
     })
 
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch('https://api:key-95837e680b6d16f52f990b2f991bd651@api.mailgun.net/v3/sandboxbbf6fecad8ad4b16b09b4853fc669703.mailgun.org')
   }
 
   render() {
@@ -41,15 +52,15 @@ export class PostCard extends Component {
             <form className="postcard-message-form">
               <div className="field">
                 <label className="postcard-label">To: </label>
-                <input type="text" name="sendEmail" placeholder="email" className="postcard-form-style"/>
+                <input type="text" name="receiverEmail" value={this.state.receiverEmail} onChange={this.handleChange} placeholder="email" className="postcard-form-style"/>
               </div>
               <div className="field">
-                <label className="postcard-label">From: </label><input type="text" name="fromEmail" placeholder={userEmail} className="postcard-form-style"/>
+                <label className="postcard-label">From: </label><input type="text" name="senderEmail" defaultValue={userEmail} placeholder={userEmail} className="postcard-form-style"/>
               </div>
               <div className="field" id="message-box">
-                <label className="postcard-label">Message: </label><input type="text" name="postcardBody" placeholder="Hello!" size="400" className="postcard-form-style"/>
+                <label className="postcard-label">Message: </label><input type="text" name="postCardBody" value={this.state.postCardBody} onChange={this.handleChange} placeholder="Hello!" size="400" className="postcard-form-style"/>
               </div>
-                <button className="ui button">Send!</button>
+                <button type="submit" className="ui button">Send!</button>
             </form>
             </div>
           </div>
