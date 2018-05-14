5300af496243273ad6fb4c80ee936c8d0a29330e
Cecilia Avery <cilsong@gmail.com>
2018-05-14 16:40:20 -0400

    receive postcard

--- pintrips/src/client/components/PostcardReceived.js
+++ pintrips/src/client/components/PostcardReceived.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-// import { Dropdown, Menu, Icon, Popup, Input, Button, List, Label, Sidebar } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import { withAuth } from 'fireview'
 import firebase from 'firebase'
 import history from '../../history'
@@ -15,7 +15,12 @@ class PostCardReceived extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentCoordinates: []
+      currentCoordinates: [],
+      receiver: '',
+      sender: '',
+      message: '',
+      date: null,
+      cardIsFront: true
     }
     this.rotate = this.rotate.bind(this)
   }
@@ -26,31 +31,46 @@ class PostCardReceived extends Component {
   //set up noti for postcard
 
   componentDidMount() {
-    console.log('props in postcard noti', this.props._auth.currentUser.email)
     const user = this.props._auth.currentUser.email;
+    const self = this;
 
-    user && db.collection('postcards').where('receiver', "==", user)
+    user && db.collection('postcards').where('receiver', "==", user).limit(1)
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach(function(doc) {
-          console.log(doc.data());
-          // this.setState({
-          //   currentCoordinates: [...this.state.currentCoordinates, [doc.messageCoordinates]]
-          // })
-      });
+          self.setState({
+            currentCoordinates: doc.data().messageCoordinates,
+            receiver: doc.data().receiver,
+            sender: doc.data().sender,
+            message: doc.data().message,
+            date: doc.data().dateSent
+          })
+        })
     })
     .catch(function(error) {
         console.log("Error getting documents: ", error);
     });
-    }
+
+    this.postcardAnimation();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    TweenLite.set(".postcard-container", {transformStyle:"preserve-3d"})
+    TweenLite.set(".postcard-back", {rotationY:-180})
+    TweenLite.set([".postcard-back", ".postcard-front"], {backfaceVisibility:"hidden"})
+  }
 
   postcardAnimation() {
     TweenLite.fromTo(".postcard-container", 3, {width:0, height:0}, {width:"80vw", height:"75vh",  ease: Bounce.easeOut});
   }
 
   rotate() {
-    this.rotateToBack();
-    this.setState({ cardIsFront: false })
+    if (this.state.cardIsFront) {
+      this.rotateToBack();
+    } else {
+     this.rotateToFront();
+    }
+    this.setState({ cardIsFront: !this.state.cardIsFront })
   }
 
   rotateToBack() {
@@ -62,32 +82,28 @@ class PostCardReceived extends Component {
   }
 
   render() {
-    if (!this.state.currentCoordinates.length)
-      return <div className="login-container">loading...</div>
+    // if (!this.state.currentCoordinates.length)
+    //   return <div className="login-container">loading...</div>
+    const sendCoordToProps = [this.state.currentCoordinates._lat, this.state.currentCoordinates._long]
     return (
-        <div className='postcard-container' onClick={this.rotate}>
+      <div className="login-container">
+        <div className='postcard-container'>
           <div className="postcard-front"
             style={{ display: this.state.cardIsFront ? 'block' : 'none'}}>
-            <PostCardMap currentCoord={this.state.currentCoordinates}/>
-            <PostCardTypeText currentCoord={this.state.currentCoordinates}/>
+            <PostCardMap currentCoord={sendCoordToProps}/>
+            <PostCardTypeText currentCoord={sendCoordToProps}/>
           </div>
           <div className="postcard-back"
                 style={{ display: this.state.cardIsFront ? 'none' : 'block'}}>
-            <PostCardMessage currentCoord={this.state.currentCoordinates}/>
+            <PostCardMessage currentCoord={sendCoordToProps}/>
           </div>
+        </div>
+        <div className="postcard-flip-button">
+          <Button onClick={this.rotate} compact basic color='orange' size="mini">Flip</Button>
+        </div>
         </div>
     )
   }
-
-    // render() {
-    //   if (!this.state.currentCoordinates.length)
-    //   return <div className="login-container">loading...</div>
-    //   return (
-    //     <div className="login-container">
-    //       <h2>PostCardReceived Component in here</h2>
-    //     </div>
-    //   )
-    // }
 
 }
 
