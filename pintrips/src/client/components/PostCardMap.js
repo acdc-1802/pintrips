e1f84dc11c478e8921a0a95ca0c34021fd8419be
Cecilia Avery <cilsong@gmail.com>
2018-05-15 17:58:13 -0400

    map could show a rocket pin but do we want that?

--- pintrips/src/client/components/PostCardMap.js
+++ pintrips/src/client/components/PostCardMap.js
@@ -6,10 +6,12 @@ export class PostCardMap extends Component {
 
   render () {
     return (
-      <Image src={`https://api.mapbox.com/styles/v1/mapbox/cj3kbeqzo00022smj7akz3o1e/static/${this.props.currentCoord[1]},${this.props.currentCoord[0]},13,0,60/700x500?access_token=pk.eyJ1IjoiY2lsYXZlcnkiLCJhIjoiY2pmMW1paDd0MTQ0bzJwb2Rtemdna2g0MCJ9.64yg764mTUOrL3p77lXGSQ`} className ="postcard-map" rounded/>
-
+      <Image src={`https://api.mapbox.com/v4/mapbox.dark/url-https%3A%2F%2Fwww.mapbox.com%2Fimg%2Frocket.png(${this.props.currentCoord[1]},${this.props.currentCoord[0]})/${this.props.currentCoord[1]},${this.props.currentCoord[0]},14/700x500.png?access_token=pk.eyJ1IjoiY2lsYXZlcnkiLCJhIjoiY2pmMW1paDd0MTQ0bzJwb2Rtemdna2g0MCJ9.64yg764mTUOrL3p77lXGSQ`} />
     )
   }
 }
 
 export default withAuth(PostCardMap)
+
+//moonlight style without pin drop
+{/* <Image src={`https://api.mapbox.com/styles/v1/mapbox/cj3kbeqzo00022smj7akz3o1e/static/${this.props.currentCoord[1]},${this.props.currentCoord[0]},14,0,60/700x500?access_token=pk.eyJ1IjoiY2lsYXZlcnkiLCJhIjoiY2pmMW1paDd0MTQ0bzJwb2Rtemdna2g0MCJ9.64yg764mTUOrL3p77lXGSQ`} className ='postcard-map' rounded/> */}
